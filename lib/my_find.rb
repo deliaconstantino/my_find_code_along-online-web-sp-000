47c158d1974ab39fb9a3ceca46328e930fc65131
Delia Constantino <dconstan92@gmail.com>
2020-10-29 15:32:42 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -5,7 +5,10 @@ def my_find(collection)
   new_array = []
 
   while i < collection.length
-    new_array << yield(collection[i])
+
+    if yield(collection[i]) == true
+      new_array << collection[i]
+    end
     i += 1
   end
 
