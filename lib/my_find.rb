61ab58a1eb56a59a2947329e81fb783377cb05be
Delia Constantino <dconstan92@gmail.com>
2020-10-29 15:27:49 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,20 @@
 require 'pry'
 
 def my_find(collection)
+  i = 0
+  
 
+  while
+    new_array << yield
+  end
+
+  if new_array.length > 0
+    new_array
+  else
+    nil
+  end
 end
+
+#using a while loop, check/find elements that match a certain condition.
+#return those that match as a new array and return nil if none of the org array match the condition
+#use yield block to check condition
